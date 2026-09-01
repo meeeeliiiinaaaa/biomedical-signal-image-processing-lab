380b940c32375a52326955af9d5479e76803fc50
MelinaRajabi <129985600+meeeeliiiinaaaa@users.noreply.github.com>
2026-09-01 23:28:54 +0330

    Delete data directory

--- data/Lab 2_data/Lab2_1/plotEEG.m
+++
@@ -1,9 +0,0 @@
-function plotEEG(X) 
-load('Electrodes') ;
-
-% Plot Data
-% Use function disp_eeg(X,offset,feq,ElecName)
-offset = max(abs(X(:))) ;
-feq = 250 ;
-ElecName = Electrodes.labels ;
-disp_eeg(X,offset,feq,ElecName);
